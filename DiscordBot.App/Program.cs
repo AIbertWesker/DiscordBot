3e0e09083840a8f8e4fae131fda6812509d06f22
AIbertWesker <mbubel999@gmail.com>
2024-10-15 20:32:01 +0200

    init

---
+++ DiscordBot.App/Program.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace DiscordBot.App
+{
+    public class Program
+    {
+        private static DiscordSocketClient _client;
+
+        public static async Task Main()
+        {
+            _client = new DiscordSocketClient();
+
+            var config = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+            };
+
+            _client = new DiscordSocketClient(config);
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            var token = "xx";
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            _client.MessageReceived += MessageReceivedAsync;
+            _client.Ready += ReadyAsync;
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private static async Task MessageReceivedAsync(SocketMessage message)
+        {
+            // The bot should never respond to itself.
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+
+            if (message.Content == "!ping")
+            {
+                // Create a new ComponentBuilder, in which dropdowns & buttons can be created.
+                var cb = new ComponentBuilder()
+                    .WithButton("Click me!", "unique-id", ButtonStyle.Primary);
+
+                // Send a message with content 'pong', including a button.
+                // This button needs to be build by calling .Build() before being passed into the call.
+                await message.Channel.SendMessageAsync("pong!", components: cb.Build());
+            }
+        }
+
+        private static Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+            return Task.CompletedTask;
+        }
+    }
+}
