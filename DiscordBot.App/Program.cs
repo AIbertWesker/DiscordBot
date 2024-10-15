780137a5c69e075e6b9b0680e71c60bc7fdfa7d3
AIbertWesker <mbubel999@gmail.com>
2024-10-15 20:35:36 +0200

    init2

--- DiscordBot.App/Program.cs
+++ DiscordBot.App/Program.cs
@@ -5,7 +5,9 @@ namespace DiscordBot.App
 {
     public class Program
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
         private static DiscordSocketClient _client;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
         public static async Task Main()
         {
@@ -18,39 +20,27 @@ namespace DiscordBot.App
 
             _client = new DiscordSocketClient(config);
 
-            //  You can assign your bot token to a string, and pass that in to connect.
-            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-            var token = "xx";
-
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+            //TODO Token z JSON
+            var token = "";
 
             await _client.LoginAsync(TokenType.Bot, token);
             _client.MessageReceived += MessageReceivedAsync;
             _client.Ready += ReadyAsync;
             await _client.StartAsync();
 
-            // Block this task until the program is closed.
             await Task.Delay(-1);
         }
 
         private static async Task MessageReceivedAsync(SocketMessage message)
         {
-            // The bot should never respond to itself.
             if (message.Author.Id == _client.CurrentUser.Id)
                 return;
 
 
             if (message.Content == "!ping")
             {
-                // Create a new ComponentBuilder, in which dropdowns & buttons can be created.
                 var cb = new ComponentBuilder()
                     .WithButton("Click me!", "unique-id", ButtonStyle.Primary);
-
-                // Send a message with content 'pong', including a button.
-                // This button needs to be build by calling .Build() before being passed into the call.
                 await message.Channel.SendMessageAsync("pong!", components: cb.Build());
             }
         }
